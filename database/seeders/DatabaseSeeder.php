b049bf5279d3502434d1d7ec1f2a9e4c846e1224
kangkbl <150355393+kangkbl@users.noreply.github.com>
2025-11-09 05:30:33 +0700

    Update DatabaseSeeder to add Super Admin user
    Comment out existing user and barang factory calls and add a new user with specific credentials.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(1000)->create();
+        // User::factory(1000)->create();
 
-        Barang::factory(200)->create();
+        // Barang::factory(200)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Super Admin',
+            'email' => 'super@superadmin.com',
+            'password' => 'SuperAdmin'
+            ]);
     }
 }
